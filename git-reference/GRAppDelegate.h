89fba5b2944fa5a360f551c963851707b5b18b23
Jeff Bennion <Jeff@Jeffs-MacBook-Pro.local>
2014-05-20 19:21:42 -0600

    Initial Commit

---
+++ git-reference/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  git-reference
+//
+//  Created by Jeff Bennion on 5/20/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git-reference/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git-reference
+//
+//  Created by Jeff Bennion on 5/20/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
